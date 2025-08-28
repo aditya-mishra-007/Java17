cf8a910a5d55a01efcaf56af190ef7c39d1b1491
Aditya_Mishra_202401100300017 <aditya-mishra-007>
2025-08-29 00:41:17 +0530

    Student1 29/08/2025

---
+++ src/MyMain.java
@@ -0,0 +1,8 @@
+public class MyMain {
+    public static void main(String[] args) {
+        SchoolStudent ss = new SchoolStudent(17,"Aditya");
+         ss.attend_class();
+         ss.give_exam();
+         ss.register();
+    }
+}
