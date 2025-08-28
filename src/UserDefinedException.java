a069b290a66743202f781984a719563b5710e9be
Aditya Mishra <adityamishra1425@gmail.com>
2025-08-28 09:51:53 +0530

    Delete src/UserDefinedException.java

--- src/UserDefinedException.java
+++
@@ -1,8 +0,0 @@
-// Step - 2 Continuation
-public class UserDefinedException {
-    public void validate(int age) throws InvalidAgeException {
-        if(age<18)
-            throw new InvalidAgeException("Not Eligible to vote age less than 18");
-        else System.out.println("Eligible to vote");
-    }
-}
