2436bfc4a8c2fb007993bf2da38fa412ceffe638
Aditya_Mishra_202401100300017 <aditya-mishra-007>
2025-08-29 00:28:52 +0530

    CollegeStudent 29/08/2025

---
+++ src/MyMain1.java
@@ -0,0 +1,7 @@
+public class MyMain1 {
+    public static void main(String[] args) {
+        CollegeStudent cs = new CollegeStudent("Delhi",17);
+        cs.show_address();
+        cs.show_roll();
+    }
+}
