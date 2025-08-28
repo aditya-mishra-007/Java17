d18bd07a9a1debcc4c28f084dcfcafabd1881815
Aditya_Mishra_202401100300017 <aditya-mishra-007>
2025-08-29 00:13:29 +0530

    Employee 29/08/2025

---
+++ src/Employee.java
@@ -0,0 +1,22 @@
+public class Employee {
+    int emp_id;
+    String emp_name;
+    double emp_salary;
+    static String emp_org = "TCS"; // when a static variable is declared it is share among all same levels in objects
+
+    public Employee(int emp_id, String emp_name, double emp_salary) {
+        this.emp_id = emp_id;
+        this.emp_name = emp_name;
+        this.emp_salary = emp_salary;
+    }
+
+    public void display(){
+        System.out.println("EMP ID: "+emp_id);
+        System.out.println("EMP NAME: "+emp_name);
+        System.out.println("EMP SALARY: "+emp_salary);
+    }
+
+    public static void emp_info(){ // if we have created the method using 'static' we can access it even without creating an object
+        System.out.println(emp_org);
+    }
+}
